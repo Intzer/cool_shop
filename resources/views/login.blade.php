42544726142b3c9c9c9424e1cb1fa968d7b36d90
roman <rmilh1@mail.ru>
2024-05-13 19:52:55 +0300

    fix login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,16 +3,25 @@
 @section('content')
     <div class="text-center d-flex align-items-center p-4">
         <main class="form-signin m-auto">
-            <form>
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
+            @if($errors->any())
+                <ul class="list-group list-group-flush">
+                    @foreach($errors->all() as $error)
+                        <li class="list-group-item text-danger">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+            <form action="{{ route('login.store') }}" method="post">
+                @csrf
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                    <label for="email">Email address</label>
                 </div>
                 <div class="form-floating my-2">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    <label for="password">Password</label>
                 </div>
 
                 <button class="w-100 btn btn-lg btn-primary my-2" type="submit">Sign in</button>
